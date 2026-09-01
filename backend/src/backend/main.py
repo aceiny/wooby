79b0a66442b55d82f379edc77d90a4c483300948
yassine <aceiny.dev@gmail.com>
2026-09-01 16:02:59 +0200

    implement database connection and configuration; add FastAPI endpoints for testing

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from internal.database import get_db
 
 app = FastAPI()
 
@@ -6,3 +9,11 @@ app = FastAPI()
 @app.get('/')
 def sayHi() : 
     return "hi"
+
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+
+    return {
+        "database": result.scalar()
+    }
